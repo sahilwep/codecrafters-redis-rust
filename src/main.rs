6a38ef905e9f1c027d7d899e8660ec679824dd6b
Sahil Sharma <sahilwep@gmail.com>
2024-03-03 23:06:40 +0530

    asdf

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,8 +8,9 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accept new connection");
+                let _message = stream.write_all(b"+PONG\r\n");
             }
             Err(e) => {
                 println!("error: {}", e);
