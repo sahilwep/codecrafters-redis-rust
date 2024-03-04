df669e707c978b41f22a15f7533dfdcab75ec8e6
Sahil Sharma <sahilwep@gmail.com>
2024-03-04 14:47:25 +0530

    using thread async

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,27 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::thread;
-use std::time::Duration;
+use std::thread;    // using thread to make this asynchronous
 
 fn main(){
     let listener = TcpListener::bind("127.0.0.1:6379").expect("Could not bind");    // create a listener server that listens on localhost port 6379
     println!("[+] TcpListener start at 127.0.0.1:6379");
     // handling incoming multiple incoming client through loop, this will make them in queue, while only one client will connect to our server.
-    let handel = thread::spawn( move || {
-        for stream in listener.incoming() {
-            match stream {  // using match as it return in "Result", to handel handel success & failure 
-                Ok(stream) => {
-                    // println!("{:?}",stream); // print this to know about the TcpStream details.
-                    
-                    // using thread 
+    for stream in listener.incoming() {
+        match stream {  // using match as it return in "Result", to handel handel success & failure 
+            Ok(stream) => {
+                // thread implementation
+                thread::spawn( || {
                     println!("\n[+] Client has connected to server!");  // simple connected message will pop on server side.
-                    // everything in here runs in separate thread
+                    // println!("{:?}",stream); // print this to know about the TcpStream details.
                     handle_client(stream);  // calling our function to handel our client
                     println!("[+] Respond Successfully!");
-                    thread::sleep(Duration::from_millis(1));
-                }
-                Err(e) => {
-                    eprint!("Server can't connect to client: {}", e);   // handling error when our server fails to connect with client.
-                }
+                });
+            }
+            Err(e) => {
+                eprint!("Server can't connect to client: {}", e);   // handling error when our server fails to connect with client.
             }
         }
-    });
-    thread::sleep(Duration::from_millis(2));
-    handel.join().unwrap(); // it will wait until our threads are over.
+    }
 }
 
 
